5e3b3e0d50a0745a64277dfbdadb78d3bd7049db
Yufeng.Meng <121055230@qq.com>
2015-06-12 11:40:03 +0800

    Initial Commit

---
+++ petKnow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  petKnow
+//
+//  Created by 孟钰丰 on 15/6/12.
+//  Copyright (c) 2015年 宠知道科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ petKnow/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  petKnow
+//
+//  Created by 孟钰丰 on 15/6/12.
+//  Copyright (c) 2015年 宠知道科技有限公司. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
